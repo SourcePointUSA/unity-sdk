ec820e6572a31795f8a794df5c4fdbf1206e613e
Fedko Dmytro <dmytro.f@gowombat.team>
2021-04-29 18:55:02 +0300

    CAMPAIGN_ENV added

---
+++ Assets/Scripts/ConsentMessagePlugin/enum/CAMPAIGN_ENV.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace GdprConsentLib
+{
+    public enum CAMPAIGN_ENV
+    {
+        STAGE,
+        PUBLIC
+    }
+
+    internal static class CAMPAIGN_ENV_STRING_KEY
+    {
+        internal const string
+        STAGE = "stage",
+        PUBLIC = "prod";
+    }
+}
