d8eec92cbfd698e77a08843371aa401161079fca
Fedko Dmytro <dmytro.f@gowombat.team>
2021-04-29 18:55:12 +0300

    CAMPAIGN_TYPE added

---
+++ Assets/Scripts/ConsentMessagePlugin/enum/CAMPAIGN_TYPE.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace GdprConsentLib
+{
+    enum CAMPAIGN_TYPE
+    {
+        GDPR,
+        CCPA,
+    }
+
+    internal static class CAMPAIGN_TYPE_STRING_KEY
+    {
+        internal const string
+        GDPR = "GDPR",
+        CCPA = "CCPA";
+    }
+}
