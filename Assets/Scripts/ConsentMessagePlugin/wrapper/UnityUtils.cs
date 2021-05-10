b89874636527c9b772b17b84cf4ecfa7841a7526
Fedko Dmytro <dmytro.f@gowombat.team>
2021-05-10 14:41:05 +0300

    usings implemented

--- Assets/Scripts/ConsentMessagePlugin/wrapper/UnityUtils.cs
+++ Assets/Scripts/ConsentMessagePlugin/wrapper/UnityUtils.cs
@@ -7,25 +7,29 @@ namespace GdprConsentLib
     {
         internal static AndroidJavaObject ConvertArrayToList(AndroidJavaObject[] array)
         {
-            AndroidJavaClass UnityUtils = new AndroidJavaClass("com.sourcepoint.cmplibrary.util.UnityUtils");
-            Util.Log("C# : passing Array to List conversion to Android's UnityUtils...");
-            AndroidJavaObject list = UnityUtils.CallStatic<AndroidJavaObject>("targetingParamArrayToList", new AndroidJavaObject[][] { array });
-            return list;
+            using (AndroidJavaClass UnityUtils = new AndroidJavaClass("com.sourcepoint.cmplibrary.util.UnityUtils"))
+            {
+                Util.Log("C# : passing Array to List conversion to Android's UnityUtils...");
+                AndroidJavaObject list = UnityUtils.CallStatic<AndroidJavaObject>("targetingParamArrayToList", new AndroidJavaObject[][] { array });
+                return list;
+            }
         }
 
         internal static Exception ConvertThrowableToError(AndroidJavaObject rawErr)
         {
-            AndroidJavaClass UnityUtils = new AndroidJavaClass("com.sourcepoint.cmplibrary.util.UnityUtils");
-            try
+            using (AndroidJavaClass UnityUtils = new AndroidJavaClass("com.sourcepoint.cmplibrary.util.UnityUtils"))
             {
-                Util.Log("C# : passing Throwable to Exception conversion to Android's UnityUtils...");
-                UnityUtils.CallStatic("throwableToException", rawErr);
+                try
+                {
+                    Util.Log("C# : passing Throwable to Exception conversion to Android's UnityUtils...");
+                    UnityUtils.CallStatic("throwableToException", rawErr);
+                }
+                catch (AndroidJavaException exception)
+                {
+                    return exception;
+                }
+                return new NotImplementedException();
             }
-            catch (AndroidJavaException exception)
-            {
-                return exception;
-            }
-            return new NotImplementedException();
         }
     }
 }
