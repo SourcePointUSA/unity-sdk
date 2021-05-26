daf2d9e8756895c8d6a7deeb9b76947ba0f41a71
Fedko Dmytro <dmytro.f@gowombat.team>
2021-05-26 17:35:43 +0300

    BroadcastExecuteEvents refactored into BroadcastEventsDispatcher and BroadcastEventsExecutor in order to fix a bug which appears only on Android device with apk which has "Development build" property enabled

--- Assets/Scripts/ConsentMessagePlugin/observer/BroadcastEventDispatcher.cs
+++ Assets/Scripts/ConsentMessagePlugin/observer/BroadcastEventDispatcher.cs
@@ -1,9 +1,13 @@
-﻿using UnityEngine.EventSystems;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
 
 namespace ConsentManagementProviderLib
 {
-    public static class BroadcastExecuteEvents
+    public static class BroadcastEventDispatcher
     {
+        public static Queue<Action> actions = new Queue<Action>();
+
         public static void Execute<T>(BaseEventData eventData, ExecuteEvents.EventFunction<T> functor) where T : IConsentEventHandler
         {
             var handlers = BroadcastReceivers.GetHandlersForEvent<T>();
@@ -11,7 +15,7 @@ namespace ConsentManagementProviderLib
             CmpDebugUtil.Log($"{typeof(T).Name} has {handlers.Count} invokable instances");
             foreach (var handler in handlers)
             {
-                ExecuteEvents.Execute<T>(handler, eventData, functor);
+                actions.Enqueue(delegate { ExecuteEvents.Execute<T>(handler, eventData, functor); });
             }
         }
     }
