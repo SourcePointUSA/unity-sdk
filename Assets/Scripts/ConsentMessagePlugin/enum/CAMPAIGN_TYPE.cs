5801539d942da5875945292c24c11eadea4247b7
Fedko Dmytro <dmytro.f@gowombat.team>
2021-06-09 11:55:33 +0300

    minor fixes

--- Assets/Scripts/ConsentMessagePlugin/enum/CAMPAIGN_TYPE.cs
+++ Assets/Scripts/ConsentMessagePlugin/enum/CAMPAIGN_TYPE.cs
@@ -9,6 +9,12 @@ namespace ConsentManagementProviderLib
         CCPA,
     }
 
+    public enum CAMPAIGN_TYPE_ANDROID
+    {
+        GDPR = 0,
+        CCPA = 1
+    }
+
     internal static class CAMPAIGN_TYPE_STRING_KEY
     {
         internal const string
