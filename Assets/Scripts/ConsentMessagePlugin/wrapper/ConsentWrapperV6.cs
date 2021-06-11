f000ae05c00a81e2c3fb6a97e1151f0fdc9d9543
Fedko Dmytro <dmytro.f@gowombat.team>
2021-06-11 14:36:10 +0300

    fix: added namespace

--- Assets/Scripts/ConsentMessagePlugin/wrapper/ConsentWrapperV6.cs
+++ Assets/Scripts/ConsentMessagePlugin/wrapper/ConsentWrapperV6.cs
@@ -1,209 +1,211 @@
-﻿using ConsentManagementProviderLib;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConsentWrapperV6
+namespace ConsentManagementProviderLib
 {
-    private AndroidJavaObject consentLib;
-    private AndroidJavaObject activity;
-    private SpClientProxy spClient;
-
-    private AndroidJavaConstruct constructor;
-    private GameObject mainThreadBroadcastEventsExecutor;
-
-    private static ConsentWrapperV6 instance;
-    public static ConsentWrapperV6 Instance
+    public class ConsentWrapperV6
     {
-        get
-        {
-            if (instance == null)
-                instance = new ConsentWrapperV6();
-            return instance;
-        }
-        private set
-        {
-            instance = value;
-        }
-    }
+        private AndroidJavaObject consentLib;
+        private AndroidJavaObject activity;
+        private SpClientProxy spClient;
 
-    ConsentWrapperV6()
-    {
-#if UNITY_ANDROID
-        if (Application.platform == RuntimePlatform.Android)
-        {
-            activity = AndroidJavaConstruct.GetActivity();
-            CmpDebugUtil.Log("Activity is OK");
-            spClient = new SpClientProxy();
-            CmpDebugUtil.Log("spClient is OK");
-            this.constructor = new AndroidJavaConstruct();
-            CmpDebugUtil.Log("AndroidJavaConstruct obj is OK");
-        }
-#endif
-    }
+        private AndroidJavaConstruct constructor;
+        private GameObject mainThreadBroadcastEventsExecutor;
 
-    private void CreateBroadcastExecutorGO()
-    {
-        if (mainThreadBroadcastEventsExecutor != null) return;
-        else
+        private static ConsentWrapperV6 instance;
+        public static ConsentWrapperV6 Instance
         {
-            mainThreadBroadcastEventsExecutor = new GameObject();
-            mainThreadBroadcastEventsExecutor.name = "CMPAndroidListenerHelper";
-            mainThreadBroadcastEventsExecutor.AddComponent<BroadcastEventsExecutor>();
-        }
-    }
-
-    public void InitializeLib(List<SpCampaign> spCampaigns, int accountId, string propertyName, MESSAGE_LANGUAGE language, long messageTimeout = 3000)
-    {
-#if UNITY_ANDROID
-        if (Application.platform == RuntimePlatform.Android)
-        {
-            CreateBroadcastExecutorGO();
-            try
+            get
             {
-                AndroidJavaObject msgLang = constructor.ConstructMessageLanguage(language);
-                AndroidJavaObject[] campaigns = new AndroidJavaObject[spCampaigns.Count];
-                foreach (SpCampaign sp in spCampaigns)
+                if (instance == null)
+                    instance = new ConsentWrapperV6();
+                return instance;
+            }
+            private set
+            {
+                instance = value;
+            }
+        }
+
+        ConsentWrapperV6()
+        {
+#if UNITY_ANDROID
+            if (Application.platform == RuntimePlatform.Android)
+            {
+                activity = AndroidJavaConstruct.GetActivity();
+                CmpDebugUtil.Log("Activity is OK");
+                spClient = new SpClientProxy();
+                CmpDebugUtil.Log("spClient is OK");
+                this.constructor = new AndroidJavaConstruct();
+                CmpDebugUtil.Log("AndroidJavaConstruct obj is OK");
+            }
+#endif
+        }
+
+        private void CreateBroadcastExecutorGO()
+        {
+            if (mainThreadBroadcastEventsExecutor != null) return;
+            else
+            {
+                mainThreadBroadcastEventsExecutor = new GameObject();
+                mainThreadBroadcastEventsExecutor.name = "CMPAndroidListenerHelper";
+                mainThreadBroadcastEventsExecutor.AddComponent<BroadcastEventsExecutor>();
+            }
+        }
+
+        public void InitializeLib(List<SpCampaign> spCampaigns, int accountId, string propertyName, MESSAGE_LANGUAGE language, long messageTimeout = 3000)
+        {
+#if UNITY_ANDROID
+            if (Application.platform == RuntimePlatform.Android)
+            {
+                CreateBroadcastExecutorGO();
+                try
                 {
-                    AndroidJavaObject typeAJO = constructor.ConstructCampaignType(sp.CampaignType);
-                    AndroidJavaObject[] paramsArray = new AndroidJavaObject[sp.TargetingParams.Count+1];
-                    //adding environment as last TargetingParam in paramsArray
-                    paramsArray[paramsArray.Length-1] = constructor.ConstructTargetingParam("campaignEnv", CSharp2JavaStringEnumMapper.GetCampaignEnvKey(sp.Environment));
-                    foreach (TargetingParam tp in sp.TargetingParams)
+                    AndroidJavaObject msgLang = constructor.ConstructMessageLanguage(language);
+                    AndroidJavaObject[] campaigns = new AndroidJavaObject[spCampaigns.Count];
+                    foreach (SpCampaign sp in spCampaigns)
                     {
-                        AndroidJavaObject param = constructor.ConstructTargetingParam(tp.Key, tp.Value);
-                        paramsArray[sp.TargetingParams.IndexOf(tp)] = param;
+                        AndroidJavaObject typeAJO = constructor.ConstructCampaignType(sp.CampaignType);
+                        AndroidJavaObject[] paramsArray = new AndroidJavaObject[sp.TargetingParams.Count + 1];
+                        //adding environment as last TargetingParam in paramsArray
+                        paramsArray[paramsArray.Length - 1] = constructor.ConstructTargetingParam("campaignEnv", CSharp2JavaStringEnumMapper.GetCampaignEnvKey(sp.Environment));
+                        foreach (TargetingParam tp in sp.TargetingParams)
+                        {
+                            AndroidJavaObject param = constructor.ConstructTargetingParam(tp.Key, tp.Value);
+                            paramsArray[sp.TargetingParams.IndexOf(tp)] = param;
+                        }
+                        AndroidJavaObject paramsList = CmpJavaToUnityUtils.ConvertArrayToList(paramsArray);
+                        AndroidJavaObject campaign = constructor.ConstructCampaign(typeAJO, paramsList, sp.CampaignType);
+                        campaigns[spCampaigns.IndexOf(sp)] = campaign;
                     }
-                    AndroidJavaObject paramsList = CmpJavaToUnityUtils.ConvertArrayToList(paramsArray);
-                    AndroidJavaObject campaign = constructor.ConstructCampaign(typeAJO, paramsList, sp.CampaignType);
-                    campaigns[spCampaigns.IndexOf(sp)] = campaign;
+                    AndroidJavaObject spConfig = constructor.ConstructSpConfig(accountId: accountId,
+                                                                               propertyName: propertyName,
+                                                                               messageTimeout: messageTimeout,
+                                                                               language: msgLang,
+                                                                               spCampaigns: campaigns);
+                    consentLib = constructor.ConsrtuctLib(spConfig: spConfig,
+                                                          activity: this.activity,
+                                                          spClient: this.spClient);
                 }
-                AndroidJavaObject spConfig = constructor.ConstructSpConfig(accountId:accountId, 
-                                                                           propertyName: propertyName,
-                                                                           messageTimeout: messageTimeout,
-                                                                           language: msgLang, 
-                                                                           spCampaigns: campaigns);
-                consentLib = constructor.ConsrtuctLib(spConfig: spConfig, 
-                                                      activity: this.activity,
-                                                      spClient: this.spClient);
+                catch (Exception e)
+                {
+                    CmpDebugUtil.LogError(e.Message);
+                }
             }
-            catch (Exception e)
+#endif
+        }
+
+        public void LoadMessage(string authId = null)
+        {
+#if UNITY_ANDROID
+            if (Application.platform == RuntimePlatform.Android)
             {
-                CmpDebugUtil.LogError(e.Message);
+                try
+                {
+                    if (string.IsNullOrEmpty(authId))
+                    {
+                        RunOnUiThread(delegate { InvokeLoadMessage(); });
+                    }
+                    else
+                    {
+                        RunOnUiThread(delegate { InvokeLoadMessageWithAuthID(authId); });
+                    }
+                }
+                catch (Exception e)
+                {
+                    CmpDebugUtil.LogError(e.Message);
+                }
+            }
+#endif
+        }
+
+        public void LoadPrivacyManager(CAMPAIGN_TYPE campaignType, string pmId, PRIVACY_MANAGER_TAB tab)
+        {
+#if UNITY_ANDROID
+            if (Application.platform == RuntimePlatform.Android)
+            {
+                try
+                {
+                    AndroidJavaObject type = constructor.ConstructCampaignType(campaignType);
+                    AndroidJavaObject privacyManagerTab = constructor.ConstructPrivacyManagerTab(tab);
+                    RunOnUiThread(delegate { InvokeLoadPrivacyManager(pmId, privacyManagerTab, type, campaignType); });
+                }
+                catch (Exception e)
+                {
+                    CmpDebugUtil.LogError(e.Message);
+                }
+            }
+#endif
+        }
+
+        public void CustomConsentGDPR(string[] vendors, string[] categories, string[] legIntCategories, Action<string> onSuccessDelegate)
+        {
+            consentLib.Call("customConsentGDPR", vendors, categories, legIntCategories, new CustomConsentClient(onSuccessDelegate));
+        }
+
+        internal void Dispose()
+        {
+            if (consentLib != null)
+            {
+                constructor.Dispose();
+                CmpDebugUtil.Log("Disposing consentLib...");
+                consentLib.Call("dispose");
+                CmpDebugUtil.Log("Disposing consentLib successfully done");
             }
         }
-#endif
-    }
 
-    public void LoadMessage(string authId = null)
-    {
-#if UNITY_ANDROID
-        if (Application.platform == RuntimePlatform.Android)
+        internal void CallShowView(AndroidJavaObject view)
         {
+            consentLib.Call("showView", view);
+            CmpDebugUtil.Log("C# : View showing passed to Android's consent lib");
+        }
+
+        internal void CallRemoveView(AndroidJavaObject view)
+        {
+            consentLib.Call("removeView", view);
+            CmpDebugUtil.Log("C# : View removal passed to Android's consent lib");
+        }
+
+        private void RunOnUiThread(Action action)
+        {
+            CmpDebugUtil.Log(">>>STARTING RUNNABLE ON UI THREAD!");
+            activity.Call("runOnUiThread", new AndroidJavaRunnable(action));
+        }
+
+        private void InvokeLoadMessage()
+        {
+            CmpDebugUtil.Log("InvokeLoadMessage() STARTING...");
             try
             {
-                if (string.IsNullOrEmpty(authId))
-                {
-                    RunOnUiThread(delegate { InvokeLoadMessage(); });
-                }
-                else
-                {
-                    RunOnUiThread(delegate { InvokeLoadMessageWithAuthID(authId); });
-                }
+                consentLib.Call("loadMessage");
+                CmpDebugUtil.Log($"loadMessage() is OK...");
             }
-            catch (Exception e)
-            {
-                CmpDebugUtil.LogError(e.Message);
-            }
+            catch (Exception ex) { CmpDebugUtil.LogError(ex.Message); }
+            finally { CmpDebugUtil.Log($"InvokeLoadMessage() DONE"); }
         }
-#endif
-    }
 
-    public void LoadPrivacyManager(CAMPAIGN_TYPE campaignType, string pmId, PRIVACY_MANAGER_TAB tab)
-    {
-#if UNITY_ANDROID
-        if (Application.platform == RuntimePlatform.Android)
+        private void InvokeLoadPrivacyManager(string pmId, AndroidJavaObject tab, AndroidJavaObject campaignType, CAMPAIGN_TYPE campaignTypeForLog)
         {
+            CmpDebugUtil.Log("InvokeLoadPrivacyManager() STARTING...");
             try
             {
-                AndroidJavaObject type = constructor.ConstructCampaignType(campaignType);
-                AndroidJavaObject privacyManagerTab = constructor.ConstructPrivacyManagerTab(tab);
-                RunOnUiThread(delegate { InvokeLoadPrivacyManager(pmId, privacyManagerTab, type, campaignType); });
+                consentLib.Call("loadPrivacyManager", pmId, tab, campaignType);
+                CmpDebugUtil.Log($"loadPrivacyManager() with {campaignTypeForLog} is OK...");
             }
-            catch (Exception e)
+            catch (Exception ex) { CmpDebugUtil.LogError(ex.Message); }
+            finally { CmpDebugUtil.Log($"InvokeLoadPrivacyManager() with {campaignTypeForLog} DONE"); }
+        }
+
+        private void InvokeLoadMessageWithAuthID(string authID)
+        {
+            CmpDebugUtil.Log("loadMessage(authId: String) STARTING...");
+            try
             {
-                CmpDebugUtil.LogError(e.Message);
+                consentLib.Call("loadMessage", authID);
             }
+            catch (Exception ex) { CmpDebugUtil.LogError(ex.Message); }
+            finally { CmpDebugUtil.Log("loadMessage(authId: String) DONE"); }
         }
-#endif
-    }
-
-    public void CustomConsentGDPR(string[] vendors, string[] categories, string[] legIntCategories, Action<string> onSuccessDelegate)
-    {
-        consentLib.Call("customConsentGDPR", vendors, categories, legIntCategories, new CustomConsentClient(onSuccessDelegate));
-    }
-
-    internal void Dispose()
-    {
-        if(consentLib!=null)
-        {
-            constructor.Dispose();
-            CmpDebugUtil.Log("Disposing consentLib...");
-            consentLib.Call("dispose");
-            CmpDebugUtil.Log("Disposing consentLib successfully done");
-        }
-    }
-
-    internal void CallShowView(AndroidJavaObject view)
-    {
-        consentLib.Call("showView", view);
-        CmpDebugUtil.Log("C# : View showing passed to Android's consent lib");
-    }
-
-    internal void CallRemoveView(AndroidJavaObject view)
-    {
-        consentLib.Call("removeView", view);
-        CmpDebugUtil.Log("C# : View removal passed to Android's consent lib");
-    }
-
-    private void RunOnUiThread(Action action)
-    {
-        CmpDebugUtil.Log(">>>STARTING RUNNABLE ON UI THREAD!");
-        activity.Call("runOnUiThread", new AndroidJavaRunnable(action));
-    }
-
-    private void InvokeLoadMessage()
-    {
-        CmpDebugUtil.Log("InvokeLoadMessage() STARTING...");
-        try
-        {
-            consentLib.Call("loadMessage");
-            CmpDebugUtil.Log($"loadMessage() is OK...");
-        }
-        catch (Exception ex) { CmpDebugUtil.LogError(ex.Message); }
-        finally { CmpDebugUtil.Log($"InvokeLoadMessage() DONE"); }
-    }
-
-    private void InvokeLoadPrivacyManager(string pmId, AndroidJavaObject tab, AndroidJavaObject campaignType, CAMPAIGN_TYPE campaignTypeForLog)
-    {
-        CmpDebugUtil.Log("InvokeLoadPrivacyManager() STARTING...");
-        try
-        {
-            consentLib.Call("loadPrivacyManager", pmId, tab, campaignType);
-            CmpDebugUtil.Log($"loadPrivacyManager() with {campaignTypeForLog} is OK...");
-        }
-        catch (Exception ex) { CmpDebugUtil.LogError(ex.Message); }
-        finally { CmpDebugUtil.Log($"InvokeLoadPrivacyManager() with {campaignTypeForLog} DONE"); }
-    }
-
-    private void InvokeLoadMessageWithAuthID(string authID)
-    {
-        CmpDebugUtil.Log("loadMessage(authId: String) STARTING...");
-        try
-        {
-            consentLib.Call("loadMessage", authID);
-        }
-        catch (Exception ex) { CmpDebugUtil.LogError(ex.Message); }
-        finally { CmpDebugUtil.Log("loadMessage(authId: String) DONE"); }
     }
 }
