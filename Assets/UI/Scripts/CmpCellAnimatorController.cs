9a1100df5138a67ec0b6279f11519924eba40435
Fedko Dmytro <dmytro.f@gowombat.team>
2021-07-19 18:10:30 +0300

    [SP-7017] CMP Cell (for ScrollRect) is now a prefab with a pretty cool scaling animation!

---
+++ Assets/UI/Scripts/CmpCellAnimatorController.cs
@@ -0,0 +1,20 @@
+﻿using Assets.UI.Scripts.Util;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CmpCellAnimatorController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private Animator animator;
+    private readonly string scaleUp = "SCALE_UP";
+    private readonly string scaleDown = "SCALE_DOWN";
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        StartCoroutine(animator.TriggerAnimation(scaleUp));
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        StartCoroutine(animator.TriggerAnimation(scaleDown));
+    }
+}
