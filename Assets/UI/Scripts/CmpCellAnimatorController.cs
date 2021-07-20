b1a4765341736dee3da6e6dcb514b868da83c867
Fedko Dmytro <dmytro.f@gowombat.team>
2021-07-20 12:55:34 +0300

    [SP-7526] CMP Cell now changes text color on focus

--- Assets/UI/Scripts/CmpCellAnimatorController.cs
+++ Assets/UI/Scripts/CmpCellAnimatorController.cs
@@ -1,20 +1,43 @@
 ﻿using Assets.UI.Scripts.Util;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class CmpCellAnimatorController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] private Animator animator;
+    [SerializeField] private List<TextColorAnimationController> graphicsList;
+
     private readonly string scaleUp = "SCALE_UP";
     private readonly string scaleDown = "SCALE_DOWN";
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         StartCoroutine(animator.TriggerAnimation(scaleUp));
+        SetActive();
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         StartCoroutine(animator.TriggerAnimation(scaleDown));
+        SetIdle();
+    }
+
+    private void SetIdle()
+    {
+        foreach(TextColorAnimationController graphic in graphicsList)
+        {
+            if(graphic.isActiveAndEnabled)
+                graphic.SetIdleState();
+        }
+    }
+
+    private void SetActive()
+    {
+        foreach (TextColorAnimationController graphic in graphicsList)
+        {
+            if (graphic.isActiveAndEnabled)
+                graphic.SetActiveState();
+        }
     }
 }
