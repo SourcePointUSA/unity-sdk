13caaa9e66089d810880e3e6df96630d03659b31
Fedko Dmytro <dmytro.f@gowombat.team>
2021-07-28 16:54:57 +0300

    [SP-7565] Manage Preferences prefab added

---
+++ Assets/UI/Scripts/CmpChangeText.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class CmpChangeText : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private Text buttonText;
+    [SerializeField] private Text changingText;
+    [SerializeField] private TextColorAnimationController changingTextController;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        buttonText.fontStyle = FontStyle.Bold;
+        changingText.text = "Cookies, device identifiers, or other information can be stored or accessed on your device for the purposes presented to you.";
+        changingTextController.SetActiveState();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        buttonText.fontStyle = FontStyle.Normal;
+        changingTextController.SetIdleState();
+        changingText.text = "";
+    }
+}
