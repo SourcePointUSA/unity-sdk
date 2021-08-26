dee6ed2dc4116a46c4deced9842c4fd89e9e50d0
Fedko Dmytro <dmytro.f@gowombat.team>
2021-08-26 12:22:25 +0300

    [SP-7667] smooth logo changing added

--- Assets/UI/Scripts/Constructor/CmpNativeImageUiController.cs
+++ Assets/UI/Scripts/Constructor/CmpNativeImageUiController.cs
@@ -1,3 +1,4 @@
+using Assets.UI.Scripts.Util;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -30,7 +31,9 @@ public class CmpNativeImageUiController : CmpLocalizationUiElement
         {
             Texture2D tex = ((DownloadHandlerTexture)request.downloadHandler).texture;
             Sprite sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(tex.width / 2, tex.height / 2));
+            yield return CompanyLogo.ChangeColor(new Color(CompanyLogo.color.r, CompanyLogo.color.g, CompanyLogo.color.b, 0f));
             CompanyLogo.sprite = sprite;
+            yield return CompanyLogo.ChangeColor(new Color(CompanyLogo.color.r, CompanyLogo.color.g, CompanyLogo.color.b, 1f));
         }
     }
 }
