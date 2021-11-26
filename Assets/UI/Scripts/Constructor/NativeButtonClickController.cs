24bed9b1b6498110e628b34f335d21dbd3f96299
Dmytro Fedko <dmytro.f@gowombat.team>
2021-11-26 17:24:16 +0200

    [sp-7826] Save & Exit action after Do Not Sell button click logic is implemented

--- Assets/UI/Scripts/Constructor/NativeButtonClickController.cs
+++ Assets/UI/Scripts/Constructor/NativeButtonClickController.cs
@@ -1,9 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NativeButtonClickController : MonoBehaviour
 {
+    private static bool dnsClicked = false;
+
+    private void Start()
+    {
+        CmpNativeButtonUiController uiController = gameObject.GetComponent<CmpNativeButtonUiController>();
+        Button butt = gameObject.GetComponent<Button>();
+        if (CmpCampaignPopupQueue.CurrentCampaignToShow() == 2 && uiController.ID.Equals("DoNotSellButton"))
+            butt.onClick.AddListener(OnDoNotSellClick);
+        else if (uiController.ID.Equals("RejectAllButton"))
+            butt.onClick.AddListener(OnRejectAllClick);
+    }
+
     public void OnAcceptAllClick()
     {
         CmpLocalizationMapper.Consent(11);
@@ -11,11 +25,24 @@ public class NativeButtonClickController : MonoBehaviour
 
     public void OnSaveAndExitClick()
     {
-        CmpLocalizationMapper.Consent(1);
+        if (CmpCampaignPopupQueue.CurrentCampaignToShow() == 2 && dnsClicked)
+            OnRejectAllClick();
+        else
+            CmpLocalizationMapper.Consent(1);
     }
 
     public void OnRejectAllClick()
     {
         CmpLocalizationMapper.Consent(13);
+    }
+
+    private void OnDoNotSellClick()
+    {
+        dnsClicked = true;
+    }
+
+    private void OnDestroy()
+    {
+        dnsClicked = false;
     }
 }
