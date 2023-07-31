b7d0959872bf9c848e8581470cfd5d59267b6ef5
André Herculano <andresilveirah@gmail.com>
2023-07-31 10:41:46 +0200

    set consent ui prensentation style to over fullscreen

--- Assets/ConsentManagementProvider/Plugins/iOS/Source/UnityController.m
+++ Assets/ConsentManagementProvider/Plugins/iOS/Source/UnityController.m
@@ -274,6 +274,7 @@
 - (void)onSPUIReady:(SPMessageViewController * _Nonnull)controller {
 //    NSLog(@"onSPUIReady");
     UIViewController *top = [UIApplication sharedApplication].keyWindow.rootViewController;
+    [controller setModalPresentationStyle:UIModalPresentationOverFullScreen];
     [top presentViewController: controller animated:YES completion: nil];
     UnitySendMessage([self getGOName], "OnConsentUIReady", "onSPUIReady from iOS!");
 }
