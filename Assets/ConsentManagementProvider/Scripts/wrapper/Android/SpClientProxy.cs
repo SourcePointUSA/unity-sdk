5d07e733abc002193ef02501e850dafbad7731a7
Dmytro Fedko <dmytro.f@gowombat.team>
2023-11-15 12:29:47 +0100

    Revert "[DIA-2808] Small fix"
    This reverts commit 97fbb99fbf73167e78547c226521a44db26f31d7.

--- Assets/ConsentManagementProvider/Scripts/wrapper/Android/SpClientProxy.cs
+++ Assets/ConsentManagementProvider/Scripts/wrapper/Android/SpClientProxy.cs
@@ -58,9 +58,6 @@ namespace ConsentManagementProviderLib.Android
         void onConsentReady(string spConsents) 
         {
             CmpDebugUtil.Log("I've reached the C# onConsentReady with json string: " + spConsents);
-             SpConsents consents = JsonUnwrapper.UnwrapSpConsentsAndroid(spConsents);
-            _spConsents = consents;
-            ConsentMessenger.Broadcast<IOnConsentReady>(consents);
         }
         
         /**
@@ -75,6 +72,7 @@ namespace ConsentManagementProviderLib.Android
             {
                 SpConsents consents = JsonUnwrapper.UnwrapSpConsentsAndroid(spConsents);
                 _spConsents = consents;
+                ConsentMessenger.Broadcast<IOnConsentReady>(consents);
                 ConsentMessenger.Broadcast<IOnConsentSpFinished>(consents);
             }
             catch (Exception e)
