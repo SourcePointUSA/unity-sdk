0426bfbcc6fc1def28faf35e5e68f6ee8000d09a
Dmytro Fedko <dmytro.f@gowombat.team>
2024-01-25 14:30:08 +0100

    [DIA-3263] Removed redundant imports

--- Assets/ConsentManagementProvider/Scripts/json/JsonUnwrapper.cs
+++ Assets/ConsentManagementProvider/Scripts/json/JsonUnwrapper.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using NewtonsoftJson = Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using UnityEngine;
 
 namespace ConsentManagementProviderLib.Json
 {
