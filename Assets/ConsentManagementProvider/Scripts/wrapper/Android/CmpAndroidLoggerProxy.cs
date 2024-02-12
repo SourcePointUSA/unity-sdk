22019ced9ac14cb3509ba2a8b38a39b2a314fa79
Dmytro <dmytro.f@gowombat.team>
2024-02-12 11:38:01 +0200

    inline metods

--- Assets/ConsentManagementProvider/Scripts/wrapper/Android/CmpAndroidLoggerProxy.cs
+++ Assets/ConsentManagementProvider/Scripts/wrapper/Android/CmpAndroidLoggerProxy.cs
@@ -22,10 +22,8 @@ namespace ConsentManagementProviderLib.Android
         * It usually identifies the class or activity where the log call occurs.
         * @param msg The message you would like logged.
         */
-        void e(string tag, string msg)
-        {
+        void e(string tag, string msg) => 
             CmpDebugUtil.Log($"E  TAG: {tag}, MSG: {msg}");
-        }
 
         /**
          * The [i] method receives contains the logic to communicate with the server
@@ -34,12 +32,10 @@ namespace ConsentManagementProviderLib.Android
          * It usually identifies the class or activity where the log call occurs.
         * @param msg The message you would like logged.
          */
-        void i(string tag, string msg)
-        {
+        void i(string tag, string msg) => 
             CmpDebugUtil.Log($"I  TAG: {tag}, MSG: {msg}");
-        }
 
-#region DEBUG ONLY
+        #region DEBUG ONLY
         /**
         * The [d] method receives contains the logic to communicate with the server
         * it is used only in DEBUG
@@ -47,10 +43,8 @@ namespace ConsentManagementProviderLib.Android
         * It usually identifies the class or activity where the log call occurs.
         * @param msg The message you would like logged.
         */
-        void d(string tag, string msg)
-        {
+        void d(string tag, string msg) => 
             CmpDebugUtil.Log($"D  TAG: {tag}, MSG: {msg}");
-        }
 
         /**
         * The [v] method receives contains the logic to communicate with the server
@@ -59,66 +53,49 @@ namespace ConsentManagementProviderLib.Android
         * It usually identifies the class or activity where the log call occurs.
         * @param msg The message you would like logged.
         */
-        void v(string tag, string msg)
-        {
+        void v(string tag, string msg) => 
             CmpDebugUtil.Log($"V  TAG: {tag}, MSG: {msg}");
-        }
-#endregion
+        #endregion
 
-#region WEB REQ / RES
+        #region WEB REQ / RES
         // Web Request Log
-        void req(string tag, string url, string type, string body)
-        {
+        void req(string tag, string url, string type, string body) => 
             CmpDebugUtil.Log($"REQ  TAG: {tag}, URL: {url}, TYPE: {type}, BODY: {body}");
-        }
 
         // Web Response Log
-        void res(string tag, string msg, string status, string body)
-        {
+        void res(string tag, string msg, string status, string body) => 
             CmpDebugUtil.Log($"RES  TAG: {tag}, MSG: {msg}, STATUS: {status}, BODY: {body}");
-        }
+        #endregion
 
-        // TODO: ADD DESCRIPTION
-        void webAppAction(string tag, string msg, AndroidJavaObject? json)
-        {
-            CmpDebugUtil.Log($"WebAppAction  TAG: {tag}, MSG: {msg}, JSON: {(json != null? json : "nil")}");
-        }
- #endregion
- 
-        // TODO: ADD DESCRIPTION
-        void nativeMessageAction(string tag, string msg, AndroidJavaObject? json)
-        {
-            CmpDebugUtil.Log($"NativeMessageAction  TAG: {tag}, MSG: {msg}, JSON: {(json != null ? json : "nil")}");
-        }
-        // TODO: ADD DESCRIPTION
-
-        void computation(string tag, string msg)
-        {
+        #region COMPUTATION
+        // Native library logs about responce computation
+        void computation(string tag, string msg) => 
             CmpDebugUtil.Log($"Computation  TAG: {tag}, MSG: {msg}");
-        }
-        // TODO: ADD DESCRIPTION
 
-        void computation(string tag, string msg, AndroidJavaObject? json)
-        {
+        void computation(string tag, string msg, AndroidJavaObject? json) => 
             CmpDebugUtil.Log($"Computation2  TAG: {tag}, MSG: {msg}, JSON: {(json != null ? json : "nil")}");
-        }
-        // TODO: ADD DESCRIPTION
 
-        void clientEvent(string _event, string msg, string content)
-        {
+        #endregion COMPUTATION
+
+        #region WebApp
+        void nativeMessageAction(string tag, string msg, AndroidJavaObject? json) => 
+            CmpDebugUtil.Log($"NativeMessageAction  TAG: {tag}, MSG: {msg}, JSON: {(json != null ? json : "nil")}");
+
+        //WebApp action on pm tab or first layer tab
+        void webAppAction(string tag, string msg, AndroidJavaObject? json) => 
+            CmpDebugUtil.Log($"WebAppAction  TAG: {tag}, MSG: {msg}, JSON: {(json != null ? json : "nil")}");
+
+        //Client action on pm tab or first layer tab
+        void clientEvent(string _event, string msg, string content) => 
             CmpDebugUtil.Log($"ClientEvent  EVENT: {_event}, MSG: {msg}, CONTENT: {content}");
-        }
-        // TODO: ADD DESCRIPTION
 
-        void pm(string tag, string url, string type, string? _params)
-        {
+        //Information about pm tabs(gdpr, ccpa)
+        void pm(string tag, string url, string type, string? _params) => 
             CmpDebugUtil.Log($"Pm  TAG: {tag}, URL: {url}, TYPE: {type}, PARAMS: {(_params != null ? _params : "nil")}");
-        }
-        
-        // TODO: ADD DESCRIPTION
-        void flm(string tag, string url, string type, AndroidJavaObject json)
-        {
+
+        //Information about preloading tabs
+        void flm(string tag, string url, string type, AndroidJavaObject json) => 
             CmpDebugUtil.Log($"Flm  TAG: {tag}, URL: {url}, TYPE: {type}, JSON: {json}");
-        }
+        #endregion WebApp
     }
 }
