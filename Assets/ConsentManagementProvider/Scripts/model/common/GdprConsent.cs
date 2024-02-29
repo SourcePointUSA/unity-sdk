3e4775f605730961da6abeff61af89021ae27d54
Dmytro <dmytro.f@gowombat.team>
2024-02-29 08:59:11 +0100

    DIA-3046 update `consentStatus` class and add `ToFullString` method

--- Assets/ConsentManagementProvider/Scripts/model/common/GdprConsent.cs
+++ Assets/ConsentManagementProvider/Scripts/model/common/GdprConsent.cs
@@ -57,6 +57,9 @@ namespace ConsentManagementProviderLib
                     sb.AppendLine($"    {category}");
             }
 
+            if(consentStatus != null)
+                sb = consentStatus.ToFullString(sb);
+
             if(googleConsentMode != null)
                 sb.AppendLine($"adStorage: {googleConsentMode.adStorage}, analyticsStorage: {googleConsentMode.analyticsStorage}, adUserData: {googleConsentMode.adUserData}, adPersonalization: {googleConsentMode.adPersonalization}");
 
@@ -126,23 +129,59 @@ namespace ConsentManagementProviderLib
             this.rejectedVendors = rejectedVendors;
             this.rejectedCategories = rejectedCategories;
         }
+
+        public StringBuilder ToFullString(StringBuilder sb)
+        {
+            sb.AppendLine("ConsentStatus:");
+            if(rejectedAny != null)
+                sb.AppendLine($"    rejectedAny: {rejectedAny}");
+            if(rejectedLI != null)
+                sb.AppendLine($"    rejectedLI: {rejectedLI}");
+            if(consentedAll != null)
+                sb.AppendLine($"    consentedAll: {consentedAll}");
+            if(consentedToAny != null)
+                sb.AppendLine($"    consentedToAny: {consentedToAny}");
+            if(vendorListAdditions != null)
+                sb.AppendLine($"    vendorListAdditions: {vendorListAdditions}");
+            if(legalBasisChanges != null)
+                sb.AppendLine($"    legalBasisChanges: {legalBasisChanges}");
+            sb.AppendLine($"    hasConsentData: {hasConsentData}");
+            
+            if(granularStatus != null)
+                sb = granularStatus.ToFullString(sb);
+
+            return sb;
+        }
     }
 
     public class GranularStatus
     {
+#nullable enable
         public string? vendorConsent;
         public string? vendorLegInt;
         public string? purposeConsent;
         public string? purposeLegInt;
         public bool? previousOptInAll;
         public bool? defaultConsent;
+        public bool? sellStatus;
+        public bool? shareStatus;
+        public bool? sensitiveDataStatus;
+        public bool? gpcStatus;
+#nullable disable
+
         public GranularStatus(
+#nullable enable
             string? vendorConsent, 
             string? vendorLegInt, 
             string? purposeConsent, 
             string? purposeLegInt, 
             bool? previousOptInAll, 
-            bool? defaultConsent)
+            bool? defaultConsent,
+            bool? sellStatus,
+            bool? shareStatus,
+            bool? sensitiveDataStatus,
+            bool? gpcStatus)
+#nullable disable
         {
             this.vendorConsent = vendorConsent;
             this.vendorLegInt = vendorLegInt;
@@ -150,6 +189,37 @@ namespace ConsentManagementProviderLib
             this.purposeLegInt = purposeLegInt;
             this.previousOptInAll = previousOptInAll;
             this.defaultConsent = defaultConsent;
+            this.sellStatus = sellStatus;
+            this.shareStatus = shareStatus;
+            this.sensitiveDataStatus = sensitiveDataStatus;
+            this.gpcStatus = gpcStatus;
+        }
+
+        public StringBuilder ToFullString(StringBuilder sb)
+        {
+            sb.AppendLine("    GranularStatus:");
+            if(vendorConsent != null)
+                sb.AppendLine($"        vendorConsent: {vendorConsent}");
+            if(vendorLegInt != null)
+                sb.AppendLine($"        vendorLegInt: {vendorLegInt}");
+            if(purposeConsent != null)
+                sb.AppendLine($"        purposeConsent: {purposeConsent}");
+            if(purposeLegInt != null)
+                sb.AppendLine($"        purposeLegInt: {purposeLegInt}");
+            if(previousOptInAll != null)
+                sb.AppendLine($"        previousOptInAll: {previousOptInAll}");
+            if(defaultConsent != null)
+                sb.AppendLine($"        defaultConsent: {defaultConsent}");
+            if(sellStatus != null)
+                sb.AppendLine($"        sellStatus: {sellStatus}");
+            if(shareStatus != null)
+                sb.AppendLine($"        shareStatus: {shareStatus}");
+            if(sensitiveDataStatus != null)
+                sb.AppendLine($"        sensitiveDataStatus: {sensitiveDataStatus}");
+            if(gpcStatus != null)
+                sb.AppendLine($"        gpcStatus: {gpcStatus}");
+            
+            return sb;
         }
     }
 }
