bcec9862a2af446c80736223f72658e86f9bb187
Dmytro <dmytro.f@gowombat.team>
2024-04-30 19:14:29 +0200

    [DIA-3799] add `ShellHelper` script to use Start/Stop Automator tasks and `startup.sh` shell script to launch multiple tests by one command

---
+++ UI-TESTS/startup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#targetWindowName=$(osascript -e 'tell app "Terminal" to do script "~/Library/Android/sdk/emulator/emulator -avd Pixel_XL_API_33"')
+#androidEmulator_window_id=$(echo ${targetWindowName} | sed 's/.*window id \([0-9]*\).*/\1/')
+#echo "Appium window ${androidEmulator_window_id}"
+
+dotnet test -s android.runsettings --filter Name=ClickAcceptAllButtonTest
+sleep 5
+dotnet test -s android.runsettings --filter Name=ClickRejecttAllButtonTest
+
+#osascript -e 'tell app "Terminal" to close window id '${androidEmulator_window_id}''
