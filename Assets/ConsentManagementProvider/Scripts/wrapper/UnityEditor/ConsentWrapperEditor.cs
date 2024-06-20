1610c1105d2decde341865aafbd043655314cbed
Dmytro <dmytro.f@gowombat.team>
2024-06-20 16:26:47 +0200

    Micro fix

--- Assets/ConsentManagementProvider/Scripts/wrapper/UnityEditor/ConsentWrapperEditor.cs
+++ Assets/ConsentManagementProvider/Scripts/wrapper/UnityEditor/ConsentWrapperEditor.cs
@@ -11,30 +11,36 @@ namespace ConsentManagementProviderLib.UnityEditor
 
         public void Initialize(int accountId, int propertyId, string propertyName, MESSAGE_LANGUAGE language, string gdprPmId, string ccpaPmId, string usnatPmId, List<SpCampaign> spCampaigns, CAMPAIGN_ENV campaignsEnvironment, long messageTimeoutInSeconds = 3, bool? transitionCCPAAuth = null, bool? supportLegacyUSPString = null) =>
             Debug.LogWarning("Emulating InitializeLib call.. Sourcepoint CMP works only for real Android/iOS devices, not the Unity Editor.");
+
         public void LoadMessage(string authId = null) =>
             Debug.LogWarning("Emulating LoadMessage call... Sourcepoint CMP works only for real Android/iOS devices, not the Unity Editor.");
+
         public void LoadPrivacyManager(CAMPAIGN_TYPE campaignType, string pmId, PRIVACY_MANAGER_TAB tab) =>
             Debug.LogWarning($"Emulating LoadPrivacyManager call for {campaignType}... " +
                              $"Sourcepoint CMP works only for real Android/iOS devices, not the Unity Editor.");
+
         public void CustomConsentGDPR(string[] vendors, string[] categories, string[] legIntCategories, Action<GdprConsent> onSuccessDelegate) =>
             Debug.LogWarning("Emulating CustomConsentGDPR call.. Sourcepoint CMP works only for real Android/iOS devices, not the Unity Editor.");
+
         public void DeleteCustomConsentGDPR(string[] vendors, string[] categories, string[] legIntCategories, Action<GdprConsent> onSuccessDelegate) =>
             Debug.LogWarning("Emulating DeleteCustomConsentGDPR call.. Sourcepoint CMP works only for real Android/iOS devices, not the Unity Editor.");
+
         public SpConsents GetSpConsents()
         {
             Debug.LogWarning("Emulating GetSpConsents call.. Sourcepoint CMP works only for real Android/iOS devices, not the Unity Editor.");
             return null;
         }
+
         public GdprConsent GetCustomConsent()
         {
             Debug.LogWarning("Emulating GetCustomConsent call.. Sourcepoint CMP works only for real Android/iOS devices, not the Unity Editor.");
             return null;
         }
+
         public void ClearAllData() =>
             Debug.LogWarning("Emulating ClearAllData call... Sourcepoint CMP works only for real Android/iOS devices, not the Unity Editor.");
+
         public void Dispose() =>
             Debug.LogWarning("Emulating Dispose call.. Sourcepoint CMP works only for real Android/iOS devices, not the Unity Editor.");
-
-
     }
 }
