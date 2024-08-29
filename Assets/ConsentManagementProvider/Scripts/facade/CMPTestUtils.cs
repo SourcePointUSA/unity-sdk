28f5f73b21b9e5d19658631a9d9568a27519320d
Dmytro <dmytro.f@gowombat.team>
2024-08-29 20:28:56 +0200

    Add initialization function by languages

--- Assets/ConsentManagementProvider/Scripts/facade/CMPTestUtils.cs
+++ Assets/ConsentManagementProvider/Scripts/facade/CMPTestUtils.cs
@@ -1,10 +1,45 @@
 using System;
+using System.Collections.Generic;
 
 namespace ConsentManagementProvider
 {
     public static class CMPTestUtils
     {
         static bool delegateCalled = false;
+
+        public static void InitializeWithLanguage(
+            string languageKey)
+        {
+            MESSAGE_LANGUAGE language = (MESSAGE_LANGUAGE)Int32.Parse(languageKey);
+
+            List<CAMPAIGN_TYPE> campaignTypes = new ();
+            List<SpCampaign> spCampaigns = new List<SpCampaign>();
+            List<TargetingParam> gdprParams = new List<TargetingParam> { new TargetingParam("location", "EU") };
+            SpCampaign gdpr = new SpCampaign(CAMPAIGN_TYPE.GDPR, gdprParams);
+            spCampaigns.Add(gdpr);
+            campaignTypes.Add(CAMPAIGN_TYPE.GDPR);
+
+            List<TargetingParam> ccpaParams = new List<TargetingParam> { new TargetingParam("location", "US") };
+            SpCampaign ccpa = new SpCampaign(CAMPAIGN_TYPE.CCPA, ccpaParams);
+            spCampaigns.Add(ccpa);
+            campaignTypes.Add(CAMPAIGN_TYPE.CCPA);
+
+            List<TargetingParam> usnatParams = new List<TargetingParam> { new TargetingParam("location", "US") };
+            SpCampaign usnat = new SpCampaign(CAMPAIGN_TYPE.USNAT, usnatParams, transitionCCPAAuth: false, supportLegacyUSPString: false);
+            spCampaigns.Add(usnat);
+            campaignTypes.Add(CAMPAIGN_TYPE.USNAT);
+
+            CMP.Instance.Initialize(
+                spCampaigns: spCampaigns,
+                accountId: 22,
+                propertyId: 16893,
+                propertyName: "mobile.multicampaign.demo",
+                language: language,
+                campaignsEnvironment: CAMPAIGN_ENV.PUBLIC,
+                messageTimeoutInSeconds: 30
+        );
+        }
+
         public static void LoadPrivacyManager(
             string campaignType, 
             string pmId)
