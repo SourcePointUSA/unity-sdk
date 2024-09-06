8b9cf50289faa302c1b915e27b0a8350851d2b5f
Dmytro <dmytro.f@gowombat.team>
2024-09-06 14:12:17 +0200

    Increase `androidInstallTimeout`

--- UI-TESTS/UITests.cs
+++ UI-TESTS/UITests.cs
@@ -31,6 +31,7 @@ namespace UnityAppiumTests
 			desiredCaps.AddAdditionalCapability("deviceName", TestContext.Parameters["deviceName"]);
 			desiredCaps.AddAdditionalCapability("appium:uiautomator2ServerInstallTimeout", 120000);
 			desiredCaps.AddAdditionalCapability("appium:uiautomator2ServerLaunchTimeout", 60000);
+			desiredCaps.AddAdditionalCapability("appium:androidInstallTimeout", 180000);
 			desiredCaps.AddAdditionalCapability("appium:app", (string)rootDir+TestContext.Parameters["appium:app"]);
 			desiredCaps.AddAdditionalCapability("appium:automationName", TestContext.Parameters["appium:automationName"]);
 			desiredCaps.AddAdditionalCapability("appium:altUnityHost", TestContext.Parameters["altTesterIP"]);
