c18d809942c4615eb7c25d2e0d5ddc988a0bd97c
Dmytro <dmytro.f@gowombat.team>
2024-09-12 17:20:11 +0200

    Add logcat

--- UI-TESTS/startup.sh
+++ UI-TESTS/startup.sh
@@ -16,84 +16,84 @@ runTest()
 }
 
 #Make sure adb server working
-adb kill-server
-adb start-server
-sleep 5
+#adb kill-server
+#adb start-server
+#sleep 5
 
 #Start Android Emulator
-targetWindowName=$(osascript -e 'tell app "Terminal" to do script "~/Library/Android/sdk/emulator/emulator -avd Pixel_XL_API_33"')
-androidEmulator_window_id=$(echo ${targetWindowName} | sed 's/.*window id \([0-9]*\).*/\1/')
-echo "Android window ${androidEmulator_window_id}"
+#targetWindowName=$(osascript -e 'tell app "Terminal" to do script "~/Library/Android/sdk/emulator/emulator -avd Pixel_XL_API_33"')
+#androidEmulator_window_id=$(echo ${targetWindowName} | sed 's/.*window id \([0-9]*\).*/\1/')
+#echo "Android window ${androidEmulator_window_id}"
 
 #Boot IOS Simulator
-targetWindowName=$(osascript -e 'tell app "Terminal" to do script "/Applications/Xcode.app/Contents/Developer/Applications/Simulator.app/Contents/MacOS/Simulator -CurrentDeviceUDID 75EBD8C0-A809-4C7C-B45D-169469835DC4"')
-iosEmulator_window_id=$(echo ${targetWindowName} | sed 's/.*window id \([0-9]*\).*/\1/')
-echo "IOS window ${iosEmulator_window_id}"
+#targetWindowName=$(osascript -e 'tell app "Terminal" to do script "/Applications/Xcode.app/Contents/Developer/Applications/Simulator.app/Contents/MacOS/Simulator -CurrentDeviceUDID 75EBD8C0-A809-4C7C-B45D-169469835DC4"')
+#iosEmulator_window_id=$(echo ${targetWindowName} | sed 's/.*window id \([0-9]*\).*/\1/')
+#echo "IOS window ${iosEmulator_window_id}"
 
 #Wait for emulators
-sleep 60
+#sleep 60
 
 #Test android
 runTest android.runsettings ClickAcceptAllButtonTest
-sleep 5
-runTest android.runsettings ClickRejectAllButtonTest
-sleep 5
-runTest android.runsettings OpenPmLayersTest
-sleep 5
-runTest android.runsettings SaveAndExitGDPRTest
-sleep 5
-runTest android.runsettings SaveAndExitCCPATest
-sleep 5
-runTest android.runsettings SaveAndExitUSNATTest
-sleep 5
-runTest android.runsettings ClearAllButtonTest
-sleep 5
-runTest android.runsettings AuthIdTest
-sleep 5
-runTest android.runsettings AcceptRejectAllUsnatInPMTest
-sleep 5
-runTest android.runsettings ProgramaticRejectAllGDPRTest
-sleep 5
-runTest android.runsettings ProgramaticRejectAllCCPATest
-sleep 5
-runTest android.runsettings ProgramaticRejectAllUSNATTest
-sleep 5
-runTest android.runsettings ProgramaticCustomConsentGDPRTest
-sleep 5
-runTest android.runsettings MessageLanguageTest
-
-#Kill android emulator
-osascript -e 'tell app "Terminal" to close window id '${androidEmulator_window_id}''
-sleep 5
-
-#Test ios
-runTest ios.runsettings ClickAcceptAllButtonTest
-sleep 5
-runTest ios.runsettings ClickRejectAllButtonTest
-sleep 5
-runTest ios.runsettings OpenPmLayersTest
-sleep 5
-runTest ios.runsettings SaveAndExitGDPRTest
-sleep 5
-runTest ios.runsettings SaveAndExitCCPATest
-sleep 5
-runTest ios.runsettings SaveAndExitUSNATTest
-sleep 5
-runTest ios.runsettings ClearAllButtonTest
-sleep 5
-runTest ios.runsettings AuthIdTest
-sleep 5
-runTest ios.runsettings AcceptRejectAllUsnatInPMTest
-sleep 5
-runTest ios.runsettings ProgramaticRejectAllGDPRTest
-sleep 5
-runTest ios.runsettings ProgramaticRejectAllCCPATest
-sleep 5
-runTest ios.runsettings ProgramaticRejectAllUSNATTest
-sleep 5
-runTest ios.runsettings ProgramaticCustomConsentGDPRTest
-sleep 5
-runTest ios.runsettings MessageLanguageTest
-
-#Kill ios emulator
-osascript -e 'tell app "Terminal" to close window id '${iosEmulator_window_id}''
+#sleep 5
+#runTest android.runsettings ClickRejectAllButtonTest
+#sleep 5
+#runTest android.runsettings OpenPmLayersTest
+#sleep 5
+#runTest android.runsettings SaveAndExitGDPRTest
+#sleep 5
+#runTest android.runsettings SaveAndExitCCPATest
+#sleep 5
+#runTest android.runsettings SaveAndExitUSNATTest
+#sleep 5
+#runTest android.runsettings ClearAllButtonTest
+#sleep 5
+#runTest android.runsettings AuthIdTest
+#sleep 5
+#runTest android.runsettings AcceptRejectAllUsnatInPMTest
+#sleep 5
+#runTest android.runsettings ProgramaticRejectAllGDPRTest
+#sleep 5
+#runTest android.runsettings ProgramaticRejectAllCCPATest
+#sleep 5
+#runTest android.runsettings ProgramaticRejectAllUSNATTest
+#sleep 5
+#runTest android.runsettings ProgramaticCustomConsentGDPRTest
+#sleep 5
+#runTest android.runsettings MessageLanguageTest
+#
+##Kill android emulator
+#osascript -e 'tell app "Terminal" to close window id '${androidEmulator_window_id}''
+#sleep 5
+#
+##Test ios
+#runTest ios.runsettings ClickAcceptAllButtonTest
+#sleep 5
+#runTest ios.runsettings ClickRejectAllButtonTest
+#sleep 5
+#runTest ios.runsettings OpenPmLayersTest
+#sleep 5
+#runTest ios.runsettings SaveAndExitGDPRTest
+#sleep 5
+#runTest ios.runsettings SaveAndExitCCPATest
+#sleep 5
+#runTest ios.runsettings SaveAndExitUSNATTest
+#sleep 5
+#runTest ios.runsettings ClearAllButtonTest
+#sleep 5
+#runTest ios.runsettings AuthIdTest
+#sleep 5
+#runTest ios.runsettings AcceptRejectAllUsnatInPMTest
+#sleep 5
+#runTest ios.runsettings ProgramaticRejectAllGDPRTest
+#sleep 5
+#runTest ios.runsettings ProgramaticRejectAllCCPATest
+#sleep 5
+#runTest ios.runsettings ProgramaticRejectAllUSNATTest
+#sleep 5
+#runTest ios.runsettings ProgramaticCustomConsentGDPRTest
+#sleep 5
+#runTest ios.runsettings MessageLanguageTest
+#
+##Kill ios emulator
+#osascript -e 'tell app "Terminal" to close window id '${iosEmulator_window_id}''
