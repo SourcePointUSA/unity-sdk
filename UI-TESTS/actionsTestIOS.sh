73b20a13bd0537eaac8d1c586731fe2b67b44322
Dmytro <dmytro.f@gowombat.team>
2024-09-18 18:52:25 +0200

    Increase rerun attempts

--- UI-TESTS/actionsTestIOS.sh
+++ UI-TESTS/actionsTestIOS.sh
@@ -2,7 +2,7 @@ appium --use-plugins=altunity &>~/work/unity-sdk/unity-sdk/logs/appiumLog.txt &
 cd ~ && AltTesterDesktop.app/Contents/MacOS/AltTesterDesktop -batchmode -nographics -port 13000 -license $1 -termsAndConditionsAccepted -logfile ~/work/unity-sdk/unity-sdk/logs/altTesterLog.txt &>~/work/unity-sdk/unity-sdk/logs/runAltTesterLog.txt &
 pidAltTester=$!
 sleep 15
-test-rerun ~/work/unity-sdk/unity-sdk/UI-TESTS -s ~/work/unity-sdk/unity-sdk/UI-TESTS/ios.runsettings --filter Name=$2
+test-rerun ~/work/unity-sdk/unity-sdk/UI-TESTS -s ~/work/unity-sdk/unity-sdk/UI-TESTS/ios.runsettings --filter Name=$2 --rerunMaxAttempts 5
 exitCode=$?
 kill -9 $pidAltTester
 sleep 15
