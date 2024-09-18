a1fe5c48d94edb3d571b1f3a76902d1a1945381d
Dmytro <dmytro.f@gowombat.team>
2024-09-18 18:03:16 +0200

    Fix log

--- UI-TESTS/actionsTestIOS.sh
+++ UI-TESTS/actionsTestIOS.sh
@@ -1,4 +1,4 @@
-appium --use-plugins=altunity &>logs/appiumLog.txt &
+appium --use-plugins=altunity &>~/work/unity-sdk/unity-sdk/logs/appiumLog.txt &
 cd ~ && AltTesterDesktop.app/Contents/MacOS/AltTesterDesktop -batchmode -nographics -port 13000 -license $1 -termsAndConditionsAccepted -logfile ~/work/unity-sdk/unity-sdk/logs/altTesterLog.txt &>~/work/unity-sdk/unity-sdk/logs/runAltTesterLog.txt &
 pidAltTester=$!
 sleep 15
