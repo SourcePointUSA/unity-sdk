802192584caa71c9ec69d1ae8d14c9e11114d286
Dmytro <dmytro.f@gowombat.team>
2024-09-18 17:54:39 +0200

    Increase sleep

--- UI-TESTS/actionsTestIOS.sh
+++ UI-TESTS/actionsTestIOS.sh
@@ -1,7 +1,9 @@
 appium --use-plugins=altunity &>logs/appiumLog.txt &
 cd ~ && AltTesterDesktop.app/Contents/MacOS/AltTesterDesktop -batchmode -nographics -port 13000 -license $1 -termsAndConditionsAccepted -logfile ~/work/unity-sdk/unity-sdk/logs/altTesterLog.txt &>~/work/unity-sdk/unity-sdk/logs/runAltTesterLog.txt &
 pidAltTester=$!
-sleep 5
+sleep 15
 test-rerun ~/work/unity-sdk/unity-sdk/UI-TESTS -s ~/work/unity-sdk/unity-sdk/UI-TESTS/android.runsettings --filter Name=$2
+exitCode=$?
 kill -9 $pidAltTester
-sleep 5
+sleep 15
+exit $exitCode
